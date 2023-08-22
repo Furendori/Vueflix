d209d860ad75d28b6cf1ef862f78fc8db76cc2b0
Julian Brasseur <brasseurjulian2@gmail.com>
2023-08-22 17:13:57 +0200

    suite exo site films

---
+++ src/views/FilmView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { onBeforeMount, ref } from 'vue'
+import FooterView from '../components/FooterView.vue'
+import { $fetch } from 'ohmyfetch';
+
+
+const api = $fetch.create({ baseURL: 'https://api.vueflix.boxydev.com/movies' })
+const getMovies = async () => {
+  return api('/api/movies', { method: 'GET' })
+}
+
+let movies = ref([])
+
+onBeforeMount(() => {
+  getMovies().then((response) => (movies.value = response))
+})
+</script>
+
+<template>
+    <div>
+        <p class="movies-title" v-for="(movie, index) in movies" :key="index"> {{ movie.original_title }}</p>
+
+    </div>
+    <FooterView></FooterView>
+</template>
+
+<style>
+    .movies-title {
+        display: flex;
+
+    }
+</style>
