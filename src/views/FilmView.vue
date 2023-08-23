b98e3bcbcd2d67afba3e2afd5ca57cba04c027b7
Julian Brasseur <brasseurjulian2@gmail.com>
2023-08-23 15:17:57 +0200

    Suite page films

--- src/views/FilmView.vue
+++ src/views/FilmView.vue
@@ -1,8 +1,7 @@
 <script setup>
 import { onBeforeMount, ref } from 'vue'
 import FooterView from '../components/FooterView.vue'
-import { $fetch } from 'ohmyfetch';
-
+import { $fetch } from 'ohmyfetch'
 
 const api = $fetch.create({ baseURL: 'https://api.vueflix.boxydev.com/movies' })
 const getMovies = async () => {
@@ -17,16 +16,55 @@ onBeforeMount(() => {
 </script>
 
 <template>
-    <div>
-        <p class="movies-title" v-for="(movie, index) in movies" :key="index"> {{ movie.original_title }}</p>
-
+  <h1 class="films">Films</h1>
+  <div class="content">
+    <div class="movie-item" v-for="(movie, index) in movies" :key="index">
+      <div class="movie-image">
+        <img :src="movie.poster_path" :alt="movie.title" />
+      </div>
+      <div class="movie-infos">
+        <p class="movie-title">{{ movie.title }}</p>
+        <p class="movie-date">{{ movie.release_date }}</p>
+      </div>
     </div>
-    <FooterView></FooterView>
+  </div>
+
+  <FooterView />
 </template>
 
 <style>
-    .movies-title {
-        display: flex;
-        flex-direction: column;
-    }
+body {
+  background-color: rgb(229, 231, 235);
+}
+
+.films {
+  text-align: center;
+  font-size: 70px;
+}
+
+.content {
+  flex-wrap: wrap;
+  margin-left: 5%;
+  display: flex;
+  flex-direction: row;
+}
+
+.movie-item {
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+  background-color: white;
+  border-radius: 10px 10px 0 0;
+}
+
+img {
+  height: 400px;
+  border-radius: 10px 10px 0 0;
+}
+
+.movie-title {
+  text-align: left;
+  margin-left: 10px;
+}
+
 </style>
